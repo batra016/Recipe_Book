3c4db7f967ffa641ee957870f0e5b8d103d429d0
batra016 <yuktibatra016@gmail.com>
2025-06-26 21:56:18 +0530

    updat1e

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Search from './components/Hero/Search';
 import Hero from './components/Hero/Hero';
@@ -8,34 +8,33 @@ import Cards from './components/Hero/Cards';
 import RecipePage from "./components/Hero/RecipePage";
 import { RecipeProvider } from './context/RecipeContext';
 import Footer from "./components/Hero/Footer";
+
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const cardsRef = useRef(null); 
 
   return (
-    <Router>
-      <RecipeProvider>
-        <main className="overflow-x-hidden">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <Search scrollRef={cardsRef} />
-                  <Hero />
-                  <Home handleCategorySelect={setSelectedCategory} />
-                  <div ref={cardsRef}>
-                    <Cards selectedCategory={selectedCategory} />
-                  </div>
-                  <Footer></Footer>
-                </>
-              }
-            />
-            <Route path="/recipe/:id" element={<RecipePage />} />
-          </Routes>
-        </main>
-      </RecipeProvider>
-    </Router>
+    <RecipeProvider>
+      <main className="overflow-x-hidden">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Search scrollRef={cardsRef} />
+                <Hero />
+                <Home handleCategorySelect={setSelectedCategory} />
+                <div ref={cardsRef}>
+                  <Cards selectedCategory={selectedCategory} />
+                </div>
+                <Footer />
+              </>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipePage />} />
+        </Routes>
+      </main>
+    </RecipeProvider>
   );
 }
 
